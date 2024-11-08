90a4121c052796ca7e86ef2a2da53b9057d57b82
ilyas lokhand <ilokhandwala786@gmail.com>
2024-11-08 11:03:19 +0530

    Update Header.jsx

--- EMS/src/Compoents/Others/Header.jsx
+++ EMS/src/Compoents/Others/Header.jsx
@@ -11,7 +11,7 @@ const Header = (props) => {
       <h1 className="text-lg sm:text-2xl font-bold">
         Hello
         <br />
-        <span className="text-xl sm:text-3xl font-semibold">Username</span>
+        <span className="text-xl sm:text-3xl font-semibold">Username👋 </span>
       </h1>
       <button
         className="bg-red-600 text-sm sm:text-base font-medium text-white px-3 py-1.5 sm:px-5 sm:py-2 rounded-sm"
